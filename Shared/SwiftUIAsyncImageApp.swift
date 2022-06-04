2ff5f033da411624ca052537852ccb79effc45b4
ShankarKakumani <shankarchowdary7545@gmail.com>
2022-06-04 07:54:50 +0530

    Initial Commit

---
+++ Shared/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  Shared
+//
+//  Created by Shankar Chowdary on 04/06/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
